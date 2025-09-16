a77b16e337f6c1ef2dd984a56973c99518f34ea0
blumenrage <b.irnberger@htlvb.at>
2025-09-16 11:59:57 +0200

    Something

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("Flawlessly executed!");
 PeriodicTimer timer = new(TimeSpan.FromSeconds(1)); 
 Console.WriteLine("Initialized timer");
+Console.WriteLine("Waiting for next tick...");
