a2a7a5ee8e589985b31d93efbe41c4c54dd8afbd
blumenrage <b.irnberger@htlvb.at>
2025-09-16 12:00:19 +0200

    HHHH

--- Program.cs
+++ Program.cs
@@ -2,3 +2,4 @@
 PeriodicTimer timer = new(TimeSpan.FromSeconds(1)); 
 Console.WriteLine("Initialized timer");
 Console.WriteLine("Waiting for next tick...");
+Console.Equals(timer.WaitForNextTickAsync().Result, true);
